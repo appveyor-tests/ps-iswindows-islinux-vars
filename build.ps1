730793083220790c7b7616df1919020d131d223f
Alena Hrynkevich <alena@appveyor.com>
2019-08-18 11:54:56 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
-Write-Host $env:windir
-Write-Host $isWindows
-Write-Host $isLinux
+Write-Host " 1 $env:windir"
+Write-Host "2 $isWindows"
+Write-Host "3 $isLinux"
 if ($env:windir -and $isWindows -ne $true) { throw '$isWindows is not defined'; }
 if ($env:windir -and $isLinux -ne $false) { throw '$isLinux is not defined'; }
