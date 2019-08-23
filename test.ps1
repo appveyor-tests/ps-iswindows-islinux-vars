11feed924e4470e70a2b923e946523f0d86e671a
Alena Hrynkevich <alena@appveyor.com>
2019-08-23 11:35:25 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,2 +1,4 @@
+echo 'if ($env:windir -and $isWindows -ne $true) { throw '$isWindows is not defined'; }'
 if ($env:windir -and $isWindows -ne $true) { throw '$isWindows is not defined'; }
+echo 'if ($env:windir -and $isLinux -ne $false) { throw '$isLinux is not defined'; }'
 if ($env:windir -and $isLinux -ne $false) { throw '$isLinux is not defined'; }
